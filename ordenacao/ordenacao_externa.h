8cf10011ce5ac1f424cd1ae920a5635e3da47799
Jhonatan Cunha <jhonatancunha@alunos.utfpr.edu.br>
2020-10-22 18:50:56 -0300

    :wastebasket: Deletando pasta extra

--- ordenacao/ordenacao_externa.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void ordenacao_externa(char *entrada, unsigned long int bytes_registros, unsigned long int bytes_buffer_saida, char *nome_saida);
-int isSaidaOrdenada (char *nome_arquivo);
